d68fdf7be30cb18b157b1e720111843e07b4baa5
chendesheng <ichendesheng@gmail.com>
2024-09-08 17:35:50 -0700

    chore

--- src/TokenDecoder.elm
+++ src/TokenDecoder.elm
@@ -1,10 +1,6 @@
 module TokenDecoder exposing (parseToken)
 
 import Base64
-import Iso8601
-import Json.Decode as Decode exposing (Decoder, field)
-import Json.Encode as Encode
-import Time
 
 
 {-| returns token payload
@@ -25,48 +21,3 @@ parseToken token =
 
     else
         Result.Err "Invalid token format"
-
-
-type alias TokenPayload =
-    { iss : String
-    , exp : Time.Posix
-    , nbf : Time.Posix
-    , jti : String
-    , thumbprint : String
-    , aud : String
-    , siteId : String
-    , agentId : String
-    }
-
-
-decodePosix : Decoder Time.Posix
-decodePosix =
-    Decode.int |> Decode.map (\seconds -> Time.millisToPosix (seconds * 1000))
-
-
-tokenPayloadDecoder : Decoder TokenPayload
-tokenPayloadDecoder =
-    Decode.map8 TokenPayload
-        (field "iss" Decode.string)
-        (field "exp" decodePosix)
-        (field "nbf" decodePosix)
-        (field "jti" Decode.string)
-        (field "thumbprint" Decode.string)
-        (field "aud" Decode.string)
-        (field "siteId" Decode.string)
-        (field "agentId" Decode.string)
-
-
-encodeTokenPayload : TokenPayload -> String
-encodeTokenPayload payload =
-    Encode.object
-        [ ( "iss", Encode.string payload.iss )
-        , ( "exp", Encode.string (Iso8601.fromTime payload.exp) )
-        , ( "nbf", Encode.string (Iso8601.fromTime payload.nbf) )
-        , ( "jti", Encode.string payload.jti )
-        , ( "thumbprint", Encode.string payload.thumbprint )
-        , ( "aud", Encode.string payload.aud )
-        , ( "siteId", Encode.string payload.siteId )
-        , ( "agentId", Encode.string payload.agentId )
-        ]
-        |> Encode.encode 0
