268a935d1c60d0ecb16d3f2de9f7db8347805c53
chendesheng <ichendesheng@gmail.com>
2025-05-17 11:54:49 +0800

    Revert "chore" Add back token decoder
    This reverts commit d68fdf7be30cb18b157b1e720111843e07b4baa5.

--- src/TokenDecoder.elm
+++ src/TokenDecoder.elm
@@ -1,6 +1,10 @@
 module TokenDecoder exposing (parseToken)
 
 import Base64
+import Iso8601
+import Json.Decode as Decode exposing (Decoder, field)
+import Json.Encode as Encode
+import Time
 
 
 {-| returns token payload
@@ -14,10 +18,69 @@ parseToken token =
                 |> String.split "."
         of
             [ _, payload, _ ] ->
-                Base64.decode payload
+                payload
+                    |> Base64.decode
+                    |> Result.andThen
+                        (\decoded ->
+                            Decode.decodeString tokenPayloadDecoder decoded
+                                |> Result.mapError Decode.errorToString
+                        )
+                    |> Result.map encodeTokenPayload
 
             _ ->
                 Result.Err "Invalid token format"
 
     else
         Result.Err "Invalid token format"
+
+
+type alias TokenPayload =
+    { iss : String
+    , exp : Time.Posix
+    , nbf : Time.Posix
+    , jti : String
+    , thumbprint : String
+    , aud : Maybe String
+    , siteId : Int
+    , agentId : String
+    }
+
+
+decodePosix : Decoder Time.Posix
+decodePosix =
+    Decode.int |> Decode.map (\seconds -> Time.millisToPosix (seconds * 1000))
+
+
+tokenPayloadDecoder : Decoder TokenPayload
+tokenPayloadDecoder =
+    Decode.map8 TokenPayload
+        (field "iss" Decode.string)
+        (field "exp" decodePosix)
+        (field "nbf" decodePosix)
+        (field "jti" Decode.string)
+        (field "thumbprint" Decode.string)
+        (Decode.maybe <| field "aud" <| Decode.string)
+        (Decode.map (String.toInt >> Maybe.withDefault 0) <| field "siteId" Decode.string)
+        (field "agentId" Decode.string)
+
+
+encodeTokenPayload : TokenPayload -> String
+encodeTokenPayload payload =
+    Encode.object
+        ([ ( "iss", Encode.string payload.iss )
+         , ( "exp", Encode.string (Iso8601.fromTime payload.exp) )
+         , ( "nbf", Encode.string (Iso8601.fromTime payload.nbf) )
+         , ( "jti", Encode.string payload.jti )
+         , ( "thumbprint", Encode.string payload.thumbprint )
+         , ( "siteId", Encode.int payload.siteId )
+         , ( "agentId", Encode.string payload.agentId )
+         ]
+            ++ (case payload.aud of
+                    Just aud ->
+                        [ ( "aud", Encode.string aud ) ]
+
+                    Nothing ->
+                        []
+               )
+        )
+        |> Encode.encode 0
