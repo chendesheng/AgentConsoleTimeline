e12b2f5bcf33f9f41aebadd9f4711b2950b55b3d
chendesheng <ichendesheng@gmail.com>
2025-12-08 14:34:10 +0800

    WIP

---
+++ components/jsonTree/keyPath.ts
@@ -0,0 +1,12 @@
+const utf8ToBase64 = (str: string) =>
+  btoa(String.fromCharCode(...new TextEncoder().encode(str)));
+const base64ToUtf8 = (b64: string) =>
+  new TextDecoder().decode(Uint8Array.from(atob(b64), (c) => c.charCodeAt(0)));
+
+export const pathToPathStr = (path: string[]) => {
+  return utf8ToBase64(JSON.stringify(path));
+};
+
+export const pathStrToPath = (pathStr: string) => {
+  return JSON.parse(base64ToUtf8(pathStr));
+};
